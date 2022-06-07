478032afc07e0be764a5fae10aa55deb50df276a
Pranjal Walia <pranjalwalia77@gmail.com>
2022-06-07 15:29:30 +0530

    feat: update root sdk interface to consume mod slb

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import 'source-map-support/register';
 
 import Oss from './modules/oss';
+import Slb from './modules/slb';
 
 export default class AliCloud {
 	public oss: Oss;
+	public slb: Slb;
 
 	private _accessKeyId: string;
 	private _accessKeySecret: string;
@@ -13,5 +15,6 @@ export default class AliCloud {
 		this._accessKeySecret = accessKeySecret;
 
 		this.oss = new Oss(accessKeyId, accessKeySecret, 'oss-cn-hangzhou');
+		this.slb = new Slb(accessKeyId, accessKeySecret, 'cn-hangzhou'); //slb.aliyuncs.com
 	}
 }
